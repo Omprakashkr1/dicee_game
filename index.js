f08148151f064d5b8a26794f9aaed431082ae263
Omprakashkr1 <omjsr29@gmail.com>
2023-12-17 21:36:12 +0530

    updated

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var randomNumber2= Math.floor(Math.random()*6)+1;
 var randpic2="images/dice"+ randomNumber2 +".png";
 document.querySelectorAll("img")[1].setAttribute("src",randpic2);
 
-if(randomNumber1<randomNumber2)document.querySelector("h1").innerHTML="player 2 wins🥇";
+
 if(randomNumber1>randomNumber2)document.querySelector("h1").innerHTML="player 1 wins🥇";
+else if(randomNumber1<randomNumber2)document.querySelector("h1").innerHTML="player 2 wins🥇";
 else document.querySelector("h1").innerHTML="draw!";
